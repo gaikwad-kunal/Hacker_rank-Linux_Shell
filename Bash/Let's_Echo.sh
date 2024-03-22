c4ca6275dff8dc6d2819f43505e9615b1c1f0b6b
gaikwad-kunal <kunalgaikwad0012@gmail.com>
2024-03-22 12:59:57 +0530

    HackerRank The linux shell bash challenges solutions

---
+++ Bash/Let's_Echo.sh
@@ -0,0 +1,24 @@
+# Write a bash script that prints the string "HELLO".
+
+# Input Format
+
+# There is no input file required for this problem.
+
+# Output Format
+
+# HELLO
+
+# Sample Input
+
+# -echo "HELLO"
+
+# Sample Output
+
+# HELLO
+
+# Explanation
+
+# -
+
+
+echo "HELLO"
